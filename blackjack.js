070224a3485c90731327e616fd65ee04a41c959a
evandocarmo <evan_hpc@hotmail.com>
2017-07-29 02:18:46 -0300

    added comments

--- blackjack.js
+++ blackjack.js
@@ -8,26 +8,25 @@ class Deck{
    constructor(){
        this.cards = new Array();
        let suits = ["spades","diamonds","clubs","hearts"];
-       for(let suit of suits){
+       for(let suit of suits){ //For every suit, generate 13 cards
            for(let rank = 1; rank < 14; rank++){
                this.cards.push(new Card(suit,rank));
            }
        }
        this.shuffle();
     }
-    shuffle(){
-        console.time(`shuffling`);
-        let max = this.cards.length;;
-        for(let [index,card] of this.cards.entries()){
+    shuffle(){ //Knuth Algorithm
+        let max = this.cards.length;
+        for(let [index,card] of this.cards.entries()){//for every card...
+            //select a random card that is equal or bigger to the current card
             let randomIndex = Math.floor(Math.random() * (max - index) + index);
-            let temp = this.cards[randomIndex];
+            let temp = this.cards[randomIndex]; //swap them
             this.cards[randomIndex] = card;
             this.cards[index] = temp;
         }
-        console.timeEnd(`shuffling`);
     }
     draw(){
-        return this.cards.splice(0,1)[0];
+        return this.cards.splice(0,1)[0]; //remove first card from top and return
     }
 }
 
@@ -55,14 +54,13 @@ class Blackjack{
         this.player = new Player();
         this.dealer = new Dealer();
     }
-    deal(receiver = this.player,quantity = 1){
+    deal(receiver = this.player,quantity = 1){ //default options deal 1 card to the player
         for(let counter = 0; counter < quantity; counter++){
-            let card = this.deck.draw();
-            if(card.rank === 1){
-                console.log(`Ace! Select 1 or 11?`);
+            let card = this.deck.draw();//draw card
+            if(card.rank === 1){ //if it's an ace...
                 receiver.points += 11; //TODO ALLOW PLAYER TO SELECT
             }
-            else if(card.rank > 10){
+            else if(card.rank > 10){//If it's a letter, it's add 10 points
                 receiver.points += 10;
             }
             else{
@@ -72,15 +70,15 @@ class Blackjack{
          }
     }
     start(){
-        this.deal(this.player,2);
+        this.deal(this.player,2);//player receives two cards
         console.log("player first hand",this.player.cards);
-        this.deal(this.dealer,2);
-        console.log("dealer's first card",this.dealer.cards[0]);
-        while(this.player.points < 21){
-            let move = this.player.decideMove();
+        this.deal(this.dealer,2);//so does dealer
+        console.log("dealer's first card",this.dealer.cards[0]);//but only first is revealed
+        while(this.player.points < 21){ //while the player hasn't busted
+            let move = this.player.decideMove(); //player decides...
             console.log("player decided to ",move);
-            if(move === "stand"){
-                console.log("dealer's move. The house hand: ",this.dealer.cards);
+            if(move === "stand"){ //dealer's move
+                console.log("dealer's move. The hole card was",this.dealer.cards[1]);
                 while(this.dealer.points < 17){
                     console.log("less than 17. Dealer hits.");
                     this.deal(this.dealer);
@@ -89,7 +87,7 @@ class Blackjack{
                 break;
             }
             else if(move === "hit"){
-                let card = this.deal(this.player);
+                let card = this.deal(this.player);//player draws one more card
                 console.log("Player hits. Draws ",card,". Hand is now: ",this.player.cards);
             }
         }
